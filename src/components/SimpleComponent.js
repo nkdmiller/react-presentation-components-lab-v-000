61d20381661037d21dc952926059ed276440c493
Nate Miller <natekmiller96@gmail.com>
2018-11-12 21:07:22 +0000

    automatically backed up by learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -5,13 +5,13 @@ state ={
   mood: "happy"
 }
 
-handleClick = () => {
-  if (this.state.mood === "happy") {
-    this.setState({ mood: "sad" });
-  } else {
-    this.setState({ mood: "happy" });
-  }
-};
+  handleClick = () => {
+    if (this.state.mood === "happy") {
+      this.setState({ mood: "sad" });
+    } else {
+      this.setState({ mood: "happy" });
+    }
+  };
   render() {
     return <div onClick={this.handleClick}>{this.state.mood}</div>
   }
